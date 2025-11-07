c5dceff195727db41da096ff40384f8eb0e54c26
GallosMarmol <165672027+GallosMarmol@users.noreply.github.com>
2025-11-07 09:22:52 -0500

    Delete SISTEMAS directory

--- SISTEMAS/JS/firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Configuración de Firebase
-const firebaseConfig = {
-      apiKey: "AIzaSyDrVwhMkg89capkPvWz-3xn3cON66gm8R0",
-      authDomain: "sistema-gallos-marmol.firebaseapp.com",
-      projectId: "sistema-gallos-marmol",
-      storageBucket: "sistema-gallos-marmol.firebasestorage.app",
-      messagingSenderId: "894078849491",
-      appId: "1:894078849491:web:f8d73397591ddc80675f26"
-};
